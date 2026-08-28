51e6411326b3fe497fc9254a4bc49c4b905427d8
Theo Wrange <253096959+theowrange@users.noreply.github.com>
2026-08-28 14:33:18 +0700

    Revert "feat: add challenge-based routing to App component"
    This reverts commit 4d6d1e2e3d21aa560f13c2e96fb05b0513272711.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import { Routes, Route } from 'react-router';
 import { routes } from './routes/routes';
 
 function App() {
-  const challenge = import.meta.env.VITE_CHALLENGE;
-
-  if (challenge) {
-    const challengeRoute = routes.find((route) => route.path === challenge);
-    if (challengeRoute) {
-      return challengeRoute.element;
-    }
-  }
-
   return (
     <Routes>
       {routes.map((route) => (
