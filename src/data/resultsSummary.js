c80409dc9e797379be1830aa003148fce29e5795
Theo Wrange <253096959+theowrange@users.noreply.github.com>
2026-09-01 14:59:18 +0700

    feat: add results summary data with associated icons

---
+++ src/data/resultsSummary.js
@@ -0,0 +1,29 @@
+import reactionIcon from '../assets/solutions/results-summary/icon-reaction.svg';
+import memoryIcon from '../assets/solutions/results-summary/icon-memory.svg';
+import verbalIcon from '../assets/solutions/results-summary/icon-verbal.svg';
+import visualIcon from '../assets/solutions/results-summary/icon-visual.svg';
+
+const resultsSummary = [
+  {
+    category: 'Reaction',
+    score: 80,
+    icon: reactionIcon,
+  },
+  {
+    category: 'Memory',
+    score: 92,
+    icon: memoryIcon,
+  },
+  {
+    category: 'Verbal',
+    score: 61,
+    icon: verbalIcon,
+  },
+  {
+    category: 'Visual',
+    score: 72,
+    icon: visualIcon,
+  },
+];
+
+export default resultsSummary;
